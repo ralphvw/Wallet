d8cfd10cd76ae2d50aa4d69b1d4d5ff1a5c5cd2f
ralphvw <williamsralph@enyata.com>
2024-01-08 15:49:28 +0000

    Controllers

--- WalletApp.API/Controllers/AuthController.cs
+++ WalletApp.API/Controllers/AuthController.cs
@@ -1,6 +1,81 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WalletApp.API.Models.Domain;
+using WalletApp.API.Models.Dto;
+using WalletApp.API.Repositories;
+
 namespace WalletApp.API.Controllers;
 
-public class AuthController
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ITokenRepository _tokenRepository;
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(UserManager<ApplicationUser> userManager, ITokenRepository tokenRepository,
+        ILogger<AuthController> logger)
+    {
+        _userManager = userManager;
+        _tokenRepository = tokenRepository;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto request)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var user = await _userManager.FindByNameAsync(request.PhoneNumber);
+        if (user != null) return Conflict(new { message = "Phone number already exists" });
+
+        var newUser = new ApplicationUser
+        {
+            UserName = request.PhoneNumber,
+            FirstName = request.FirstName,
+            LastName = request.LastName
+        };
+
+        var result = await _userManager.CreateAsync(newUser, request.Password);
+
+        if (!result.Succeeded)
+        {
+            var errors = result.Errors.Select(e => e.Description).ToList();
+
+            foreach (var error in errors)
+            {
+                _logger.LogError($"User creation error: {error}");
+            }
+
+            return BadRequest("Something went wrong");
+        }
+
+        return Ok("User was registered. Please login");
+    }
+
+    [HttpPost]
+    [Route("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto request)
+    {
+        var user = await _userManager.FindByNameAsync(request.PhoneNumber);
+
+        if (user != null)
+        {
+            var checkPassword = await _userManager.CheckPasswordAsync(user, request.Password);
+
+            if (checkPassword)
+            {
+                var jwtToken = _tokenRepository.CreateJwtToken(user);
+                var response = new TokenDto()
+                {
+                    JwtToken = jwtToken
+                };
+                return Ok(response);
+
+            }
+        }
+
+        return BadRequest("Invalid login");
+    }
 }
---
+++ WalletApp.API/Controllers/WalletsController.cs
@@ -0,0 +1,85 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WalletApp.API.Models.Domain;
+using WalletApp.API.Models.Dto;
+using WalletApp.API.Repositories;
+
+namespace WalletApp.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class WalletsController: ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IWalletRepository _walletRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<WalletsController> _logger;
+
+    public WalletsController(UserManager<ApplicationUser> userManager, IWalletRepository walletRepository,
+        IMapper mapper, ILogger<WalletsController> logger)
+    {
+        _userManager = userManager;
+        _walletRepository = walletRepository;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateWallet([FromBody] AddWalletDto request)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        var isDuplicate = await _walletRepository.CheckDuplicateWallet(request.Type, request.AccountNumber, user.Id);
+        if (isDuplicate) return Conflict(new { message = "This wallet already exists" });
+        var exceededLimit = await _walletRepository.CheckUserLimit(user.Id, 5);
+        if (exceededLimit) return BadRequest("You have exceeded your wallet limit.");
+        var wallet = new Wallet
+        {
+            AccountNumber = request.AccountNumber.Substring(0, 6),
+            AccountScheme = request.AccountScheme.ToLower(),
+            Name = request.Name,
+            CreatedAt = DateTime.UtcNow,
+            OwnerId = user.Id,
+            Type = request.Type.ToLower()
+        };
+
+        wallet = await _walletRepository.CreateWalletAsync(wallet);
+        return Ok(_mapper.Map<WalletResponseDto>(wallet));
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<IActionResult> DeleteWallet([FromRoute] int id)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var wallet = await _walletRepository.GetWalletAsync(id);
+        if (wallet == null) return NotFound();
+        if (!_walletRepository.IsWalletOwner(wallet, user.Id)) return Forbid();
+        await _walletRepository.DeleteWalletAsync(wallet);
+        return Ok(_mapper.Map<WalletResponseDto>(wallet));
+    }
+    
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<IActionResult> GetWallet([FromRoute] int id)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var wallet = await _walletRepository.GetWalletAsync(id);
+        if (wallet == null) return NotFound();
+        if (!_walletRepository.IsWalletOwner(wallet, user.Id)) return Forbid();
+        return Ok(_mapper.Map<WalletResponseDto>(wallet));
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetUserWallets()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var wallets = await _walletRepository.GetAllWalletsAsync(user.Id);
+        return Ok(_mapper.Map<List<WalletResponseDto>>(wallets));
+    }
+    
+}
